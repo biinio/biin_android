b2b8fe65cfade19fbc20b1abab2c8ed8a33890bd
RamirezAllanBiin <aramirez@biin.io>
2016-05-16 09:02:59 -0600

    Development
    Trabajar con la base de datos de desarrollo

--- Biin/app/src/main/java/com/biin/biin/Managers/BNNetworkManager.java
+++ Biin/app/src/main/java/com/biin/biin/Managers/BNNetworkManager.java
@@ -8,14 +8,14 @@ public class BNNetworkManager {
     private static BNNetworkManager ourInstance = new BNNetworkManager();
     private static boolean production = true;
 
-    private static String URL_BASE = "https://www.biin.io";
+    private static String URL_BASE = "https://dev-biin-backend.herokuapp.com";
     private static String URL_BASE_DEV = "https://www.biin.io"; // TODO usar el server de pruebas
 
     private static String URL_GET_BIINIES = "/mobile/biinies";
     private static String URL_GET_INITIALDATA = "/mobile/initialData";
 
-    private static String URL_GET_BIINIES_TEST = URL_GET_BIINIES + "/25e854b6-b5ee-483b-80fd-7aa4c85eb386";
-    private static String URL_GET_INITIALDATA_TEST = URL_GET_INITIALDATA + "/25e854b6-b5ee-483b-80fd-7aa4c85eb386/9.73854872449546/-83.9987999326416";
+    private static String URL_GET_BIINIES_TEST = URL_GET_BIINIES + "/14804ad3-9cfe-419e-a360-262551331491";
+    private static String URL_GET_INITIALDATA_TEST = URL_GET_INITIALDATA + "/14804ad3-9cfe-419e-a360-262551331491/9.84076469672071/-83.9502626783081";
 
     protected static BNNetworkManager getInstance() {
         return ourInstance;
